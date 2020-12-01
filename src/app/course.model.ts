1c716bc2b8b98f92d2722af0ed4fe28e88f0f3a8
Anya Wynne <avkwynne@gmail.com>
2020-11-30 19:55:54 -0600

    Courses class defined

---
+++ src/app/course.model.ts
@@ -0,0 +1,21 @@
+import { stripGeneratedFileSuffix } from '@angular/compiler/src/aot/util';
+
+export class Course {
+    title: string;
+    code: string;
+    days: string[];
+    startTime: string;
+    endTime: string;
+    credits: number;
+
+    constructor(title: string, code: string, days: string[], startTime: string, endTime: string, credits: number){
+        this.title = title,
+        this.code = code,
+        this.days = days,
+        this.startTime = startTime, 
+        this.endTime = endTime,
+        this.credits = credits
+    }
+
+
+}
